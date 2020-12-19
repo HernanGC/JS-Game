8d425ba9b3b30bc83cecdb898fb24c1083110cb4
Hernan <hernangonzzalez@gmail.com>
2020-12-19 19:34:47 -0300

    Adding extra functionality

--- script.js
+++ script.js
@@ -3,10 +3,22 @@
 const numberEl = document.querySelector('.number');
 const messageEl = document.querySelector('.message');
 const bodyEl = document.querySelector('body');
+let startingScore = 20;
+let score = document.querySelector('.score');
 
+const randomizer = function () {
+    return String(Math.floor(Math.random() * 20));
+}
 
+const startOver = function () {
+    numberEl.textContent = '?';
+    document.querySelector('.guess').value = '';
+    messageEl.textContent = 'Start guessing...';
+    bodyEl.style.backgroundColor = '#222';
+    randomNumber = randomizer();
+}
 
-let randomNumber = String(Math.floor(Math.random() * 20));
+let randomNumber = randomizer();
 console.log(randomNumber);
 document.querySelector('.check').addEventListener('click', function () {
     let guess = document.querySelector('.guess').value;
@@ -15,19 +27,18 @@ document.querySelector('.check').addEventListener('click', function () {
     if (guess && String(guess) == String(randomNumber)) {
         win = true;
         console.log('coso');
+    } else {
+        score.textContent -= 1;
     }
     if (win) {
         console.log('cosoagain');
         numberEl.textContent = randomNumber;
         messageEl.textContent = 'You win!';
         bodyEl.style.backgroundColor = '#60b347';
+        return;
     }
 });
 
-document.querySelector('.again').addEventListener('click', function () {
-    const counter = [1, 2, 3];
-    numberEl.textContent = '?';
-    document.querySelector('.guess').value = '';
-    messageEl.textContent = 'Start guessing...';
-    bodyEl.style.backgroundColor = '#222';
-});
+
+
+document.querySelector('.again').addEventListener('click', startOver());
